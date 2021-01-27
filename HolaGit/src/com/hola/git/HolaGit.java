455aa03a2b9c21da44f83cc57ca53058343746d4
Onetec <Onetec@ONETEC095>
2021-01-27 16:33:21 +0100

    Primer commit 1.0

---
+++ HolaGit/src/com/hola/git/HolaGit.java
@@ -0,0 +1,17 @@
+package com.hola.git;
+
+public class HolaGit {
+	
+	private String holaGit;
+
+	public String getHolaGit() {
+		return holaGit;
+	}
+
+	public void setHolaGit(String holaGit) {
+		this.holaGit = holaGit;
+	}
+	
+	
+
+}
